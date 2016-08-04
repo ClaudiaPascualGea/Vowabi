3c38cb1becac2071988d905d3b615cd7a2911969
Claudia Pascual Gea <clau.p.gea@gmail.com>
2016-08-04 21:16:00 +0200

    Line height

--- js/texts.js
+++ js/texts.js
@@ -31,9 +31,17 @@ function addTextBar(){
   sel.addEventListener("click", function(){
     var value = this.options[this.selectedIndex].value;
     var currentValue = getStyle(element, 'font-size').replace("px", "");
-    console.log(currentValue + " " + idelement + " " + value);
     if(value && idelement && currentValue!=value)
       changeCSS("font-size", value + "px", idelement);
+  });
+
+  //LineHeight Listener
+  var sel = document.querySelector(".textBar select[name='line-height']");
+  sel.addEventListener("click", function(){
+    var value = this.options[this.selectedIndex].value;
+    var currentValue = getStyle(element, 'line-height').replace("px", "");
+    if(value && idelement && currentValue!=value)
+      changeCSS("line-height", value + "px", idelement);
   });
 
 }
@@ -43,11 +51,11 @@ function removeTextBar(){
 
   setTimeout(function(){
     var activeElement = document.activeElement.parentNode;
-    if(textBar && textBar.parentNode && (activeElement.className!="textBar") ){
+    var parent = activeElement.parentNode.className;
+    if(textBar && textBar.parentNode && (activeElement.className!="textBar") && parent!="textBar"){
       textBar.parentNode.removeChild(textBar);
     }
   }, 100);
-
 
 }
 
@@ -58,29 +66,50 @@ function createTextToolbar(element){
 
   var html = "";
 
-  html += "   <button onclick='changeTextCss(this)' data-key='font-weight' data-value='bold' class='btn-small btn'><i class='icon-bold-1'></i></button>";
-  html += "   <button onclick='changeTextCss(this)' data-key='font-style' data-value='italic' class='btn-small btn'><i class='icon-italic'></i></button>";
-  html += "   <button onclick='changeTextCss(this)' data-key='text-decoration' data-value='underline' class='btn-small btn'><i class='icon-underline'></i></button>";
-  html += "   <button onclick='changeTextCss(this)' data-key='text-decoration' data-value='line-through' class='btn-small btn'><i class='icon-strike'></i></button>";
+  html += "   <button onclick='changeTextCss(this)' data-key='font-weight' data-value='bold' class='btn-small btn tooltip' data-title='Negrita'><i class='icon-bold-1'></i></button>";
+  html += "   <button onclick='changeTextCss(this)' data-key='font-style' data-value='italic' class='btn-small btn tooltip' data-title='Cursiva'><i class='icon-italic'></i></button>";
+  html += "   <button onclick='changeTextCss(this)' data-key='text-decoration' data-value='underline' class='btn-small btn tooltip' data-title='Subrayado'><i class='icon-underline'></i></button>";
+  html += "   <button onclick='changeTextCss(this)' data-key='text-decoration' data-value='line-through' class='btn-small btn tooltip' data-title='Tachado'><i class='icon-strike'></i></button>";
 
-  html += "   <button onclick='changeTextCss(this)' data-key='text-align' data-value='left' class='btn-small btn'><i class='icon-align-left'></i></button>";
-  html += "   <button onclick='changeTextCss(this)' data-key='text-align' data-value='center' class='btn-small btn'><i class='icon-align-center'></i></button>";
-  html += "   <button onclick='changeTextCss(this)' data-key='text-align' data-value='right' class='btn-small btn'><i class='icon-align-right'></i></button>";
-  html += "   <button onclick='changeTextCss(this)' data-key='text-align' data-command='justifyFull' class='btn-small btn'><i class='icon-align-justify'></i></button>";
+  html += "   <button onclick='changeTextCss(this)' data-key='text-align' data-value='left' class='btn-small btn tooltip' data-title='Texto a la izquierda'><i class='icon-align-left'></i></button>";
+  html += "   <button onclick='changeTextCss(this)' data-key='text-align' data-value='center' class='btn-small btn tooltip' data-title='Texto centrado'><i class='icon-align-center'></i></button>";
+  html += "   <button onclick='changeTextCss(this)' data-key='text-align' data-value='right' class='btn-small btn tooltip' data-title='Texto a la derecha'><i class='icon-align-right'></i></button>";
+  html += "   <button onclick='changeTextCss(this)' data-key='text-align' data-command='justifyFull' class='btn-small btn tooltip' data-title='Texto justificado'><i class='icon-align-justify'></i></button>";
 
   //Font size
   var fontSize = getStyle(element, 'font-size');
   var fs = fontSize.replace("px", "");
-  html += " <select class='styled-select btn-small btn' name='font-size'>";
-  for (var i = 8; i <= 52; i++) {
+  html += "<i class='pre-select icon-fontsize'></i>";
+  html += "<div class='styled-select tooltip' data-title='Tamaño de letra'>";
+    html += " <select name='font-size'>";
+    for (var i = 8; i <= 52; i++) {
 
-    var cl = "";
-    if (i == fs)
-      cl = "selected"
+      var cl = "";
+      if (i == fs)
+        cl = "selected"
 
-    html += " <option "+cl+">" + i + "</option>";
-  }
-  html += " </select>";
+      html += " <option "+cl+">" + i + "</option>";
+    }
+    html += " </select>";
+  html += "</div>";
+
+  //Line Height
+  var fontSize = getStyle(element, 'line-height');
+  var fs = fontSize.replace("px", "");
+  html += "<i class='pre-select icon-text-height'></i>";
+  html += "<div class='styled-select tooltip' data-title='Interlineado'>";
+    html += " <select name='line-height'>";
+    for (var i = 8; i <= 80; i++) {
+
+      var cl = "";
+      if (i == fs)
+        cl = "selected"
+
+      html += " <option "+cl+">" + i + "</option>";
+    }
+    html += " </select>";
+  html += "</div>";
+
 
   div.innerHTML = html;
  
