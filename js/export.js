5340474cfc6fa61568f0ae6491498f0985677128
Claudia Pascual Gea <clau.p.gea@gmail.com>
2016-08-27 18:42:58 +0200

    CSS Reset en export

--- js/export.js
+++ js/export.js
@@ -164,10 +164,13 @@ function pintarHijosExport(hijos, padre){
 	}
 }
 
+var cssReset = "html, body, div, span, applet, object, iframe,h1, h2, h3, h4, h5, h6, p, blockquote, pre,a, abbr, acronym, address, big, cite, code,del, dfn, em, img, ins, kbd, q, s, samp,small, strike, strong, sub, sup, tt, var,b, u, i, center,dl, dt, dd, ol, ul, li,fieldset, form, label, legend,table, caption, tbody, tfoot, thead, tr, th, td,article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video, main {margin: 0;padding: 0;border: 0;font: inherit;}";
+
 function setCSSExport(CSS, id){
 
 	cssDOMExport[id] = parseCSS(CSS);
 	var css = '';
+	css += cssReset;
 	var v =  CSS.split('--');
 	for(var i=1; i<v.length; i=i+2){
 
@@ -180,8 +183,11 @@ function setCSSExport(CSS, id){
 		css += '}';
 		
 	}
+
 	doc.getElementById("exportCSS").innerHTML += css;
 }
+
+
 
 function setCSSWidthExport(CSS, id, width){
 
